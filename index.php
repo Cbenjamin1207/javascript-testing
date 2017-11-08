e4c9c8ce1bd6940e2355a7eac13c9b990b0f3a24
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-08 15:29:49 -0700

    Created index and outline for html

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Bootstrap Challenge</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<p class="card-text">
+								Alex taco  vegas potato  taco  burrito alex, potato  magic kingdom potato  potato  greek. Taco  taco  potato  alex,
+								greek burrito book now gatlinburg book now burrito taco . Book now potato  greek burrito alex taco  potato  greek.
+								Alex magic kingdom vegas greek magic kingdom gatlinburg book now gatlinburg alex. Gatlinburg gatlinburg potato  potato
+								burrito book now burrito, magic kingdom vegas gatlinburg greek alex. Gatlinburg vegas burrito gatlinburg, potato
+								gatlinburg magic kingdom magic kingdom burrito burrito vegas. Burrito vegas book now potato  magic kingdom taco  potato
+								magic kingdom magic kingdom, gatlinburg vegas alex burrito burrito. Alex greek vegas, book now burrito magic kingdom book now
+								alex alex alex taco  taco . Greek alex alex, magic kingdom alex burrito gatlinburg greek gatlinburg greek book now.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<p class="card-text">
+								Gatlinburg book now greek magic kingdom book now vegas alex alex taco ,
+								magic kingdom vegas greek taco . Vegas greek greek, book now alex magic kingdom magic kingdom potato
+								greek burrito vegas greek alex. Vegas vegas burrito vegas potato  alex book now. Gatlinburg burrito
+								burrito book now book now taco  gatlinburg taco , taco  gatlinburg vegas potato  greek potato . Magic kingdom
+								alex greek burrito greek vegas greek gatlinburg.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<p class="card-text">
+								Greek taco  gatlinburg book now vegas greek alex, burrito book now potato  alex gatlinburg. Burrito burrito
+								magic kingdom magic kingdom magic kingdom magic kingdom alex burrito. Burrito potato  potato  alex taco  vegas
+								book now potato  greek.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
