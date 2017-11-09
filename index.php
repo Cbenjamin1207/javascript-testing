02af5c8f082e292f83f5863ef81d6e638a7098a3
Calder Benjamin <calderbenjamin@gmail.com>
2017-11-08 20:34:06 -0700

    Made find and replace work

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
-		<script src="scripts.js"></script>
+		<script type="text/javascript" rel="script" src="scripts.js" async></script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
@@ -21,12 +21,25 @@
 					<div class="card">
 						<div class="card-body">
 							<h1>Find & Replace</h1>
-							<p class="card-text" id="find">
+							<p class="card-text" id="find-replace">
 								Linda jersey royal annabelle bannock russet agata blue congo. Flava almond maris piper blue congo cara yukon gold.
 								Linda chelina golden wonder red norland alturas russet norkotah. Megachip vitelotte langlade pink eye, vitelotte up to
 								date estima blue congo red lasoda sieglinde. Marfona french fries apline ranger russet cabritas. Pike cabritas ranger russet
 								bintje nicola. Kennebec adirondack red almond primura german butterball yukon gold golden wonder.
 							</p>
+							<form action="">
+								<div class="row">
+									<div class="col">
+										<input id="find" type="text" class="form-control" placeholder="Find">
+									</div>
+								</div>
+								<div class="col">
+									<input id="replace" type="text" class="form-control" placeholder="Replace">
+								</div>
+								<div class="col">
+									<button type="button" class="btn btn-dark" onclick="replaceWord()">Replace!</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-function replaceWord(find, replace) {
-	var html = document.getElementById("find");
-	var newHtml = html.replace(find, replace);
-	document.getElementById("find").innerHTML = newHtml;
+function replaceWord() {
+	var html = document.getElementById("find-replace").innerHTML;
+	var find = document.getElementById("find");
+	var replace = document.getElementById("replace");
+	var newHtml = html.replace(find.value, replace.value);
+	document.getElementById("find-replace").innerHTML = newHtml;
 }
